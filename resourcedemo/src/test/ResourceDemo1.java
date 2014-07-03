22e834298bdd8e4a6ca08ed5ecf44a415a79e7da
shifeiluo <openvcube@gmail.com>
2014-07-03 09:28:31 +0800

    Fix Bug

--- resourcedemo/src/test/ResourceDemo1.java
+++ resourcedemo/src/test/ResourceDemo1.java
@@ -31,29 +31,29 @@ public class ResourceDemo1 {
 		
 		//在classpath根路径查找resource1.xml
 		Resource res = new ClassPathResource("resource1.xml");
-		new XmlBeanFactory(res).getBean("testBean");
+		new XmlBeanFactory(res).getBean("testBean1");
 		
 		//在test目录定位resource2.xml
 		res = new ClassPathResource("test/resource2.xml");
-		new XmlBeanFactory(res).getBean("testBean");
+		new XmlBeanFactory(res).getBean("testBean2");
 		
 		//在TestBean类所在目录定位resource2.xml
 		res = new ClassPathResource("resource2.xml", TestBean.class);
-		new XmlBeanFactory(res).getBean("testBean");
+		new XmlBeanFactory(res).getBean("testBean2");
 
 		//直接传入文件路径
-		res = new FileSystemResource("D:/eclipse/workspace/resourcedemo/src/test/resource2.xml");
-		new XmlBeanFactory(res).getBean("testBean");
+		res = new FileSystemResource("D:/sts-bundle/workspace/springframework40/resourcedemo/src/test/resource2.xml");
+		new XmlBeanFactory(res).getBean("testBean2");
 		
 		//将构建的File Handler传入到FileSystemResource中
-		File file = new File("D:/eclipse/workspace/resourcedemo/src/resource1.xml");
+		File file = new File("D:/sts-bundle/workspace/springframework40/resourcedemo/src/resource1.xml");
 		res = new FileSystemResource(file);
-		new XmlBeanFactory(res).getBean("testBean");
+		new XmlBeanFactory(res).getBean("testBean1");
 		
 		try{
 			//支持file:、http:、ftp:、等前缀，这些都是标准的URL格式
-			res = new UrlResource("file:D:/eclipse/workspace/resourcedemo/src/resource1.xml");
-			new XmlBeanFactory(res).getBean("testBean");
+			res = new UrlResource("file:D:/sts-bundle/workspace/springframework40/resourcedemo/src/resource1.xml");
+			new XmlBeanFactory(res).getBean("testBean1");
 		} catch(MalformedURLException mue){
 			log.error("", mue);
 		}
