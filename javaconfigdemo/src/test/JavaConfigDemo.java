80a13cfb57b8414c4aa85b6390c1c2ab64654da0
shifeiluo <openvcube@gmail.com>
2014-07-03 17:53:06 +0800

    Fix Bug

--- javaconfigdemo/src/test/JavaConfigDemo.java
+++ javaconfigdemo/src/test/JavaConfigDemo.java
@@ -2,49 +2,51 @@ package test;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.config.java.annotation.Bean;
-import org.springframework.config.java.annotation.Configuration;
-import org.springframework.config.java.context.JavaConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 
  * <pre>
  * 程序的中文名称。
  * </pre>
+ * 
  * @author http://www.open-v.com
  * @version 1.00.00
- * <pre>
+ * 
+ *          <pre>
  * 修改记录
- *    修改后版本:     修改人：  修改日期:     修改内容: 
+ *    修改后版本:     修改人：  修改日期:     修改内容:
  * </pre>
  */
 @Configuration
 public class JavaConfigDemo {
 
-	private static final Log log = 
-		LogFactory.getLog(JavaConfigDemo.class);
+	private static final Log log = LogFactory.getLog(JavaConfigDemo.class);
 
 	@Bean
-	public IHelloWorld helloWorld(){
+	public IHelloWorld helloWorld() {
 		HelloWorld hw = new HelloWorld();
 		hw.setHelloStr(helloStr());
 		return hw;
 	}
-	
+
 	@Bean
-	public IHelloStr helloStr(){
+	public IHelloStr helloStr() {
 		return new FileHelloStrImpl("helloworld.properties");
 	}
-	
+
 	public static void main(String[] args) {
-		//从注解中装载DI容器配置元数据
-		JavaConfigApplicationContext context = 
-				new JavaConfigApplicationContext(JavaConfigDemo.class);
-		
-		//获得受管Bean
+		// 从注解中装载DI容器配置元数据
+		ApplicationContext context = new AnnotationConfigApplicationContext(
+				JavaConfigDemo.class);
+
+		// 获得受管Bean
 		IHelloWorld hw = context.getBean(IHelloWorld.class);
 
-		//返回字符串
+		// 返回字符串
 		log.info(hw.getContent());
 	}
 
