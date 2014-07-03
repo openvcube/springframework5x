1c5e2bf43da2fbb89137d3813abd57a4e0d2e0c1
shifeiluo <openvcube@gmail.com>
2014-07-03 23:26:41 +0800

    新增SpEL内容

---
+++ speldemo/src/test/TestBean4.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.springframework.util.Assert;
+
+/**
+ * 
+ * <pre>
+ * 程序的中文名称。
+ * </pre>
+ * @author http://www.open-v.com
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public class TestBean4 {
+
+	private TestBean5 testBean5;
+
+	public TestBean4() {
+		String proper = System.getProperty("dependson");
+		Assert.hasText(proper);
+	}
+
+	public TestBean5 getTestBean5() {
+		return testBean5;
+	}
+
+	public void setTestBean5(TestBean5 testBean5) {
+		this.testBean5 = testBean5;
+	}
+
+}
